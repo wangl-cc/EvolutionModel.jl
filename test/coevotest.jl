c09e502217dde271dd45daf4a49a453b1d8e2247
Long Wang <wangl.cc@outlook.com>
2019-04-18 13:04:16 +0800

    Add test.

---
+++ test/coevotest.jl
@@ -0,0 +1,24 @@
+using EvolutionModel
+using Plots
+
+plotlyjs()
+
+function mutfunc(b, d, c, i)
+    b = vcat(b, b[i])
+    d = vcat(d, d[i])
+    c = EvolutionModel.mutcompmat(c, i)
+    return b, d, c
+end
+
+m = CoevoCompModel([100], [0.6], [0.1], ones(1,1)*0.005, 0.05, mutfunc)
+
+
+p, t = gillespie(m, 100)
+
+plt = plot(t, [get(i, 1, 0) for i in p], legend=false)
+for j in 2:length(p[end])
+    plot!(plt, t, [get(i, j, 0) for i in p], )
+end
+
+i = rand(Int)
+png(plt, joinpath(@__DIR__, "plt-$i.png"))
